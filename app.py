a96521fb15d748bb91245e8776b707884b83f5dd
pawelsmykla <pawelsmykla@gmail.com>
2025-04-08 01:10:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import sys
-import os
-
 from flask import Flask, render_template, request
 from generator import generuj_wszystkie_warianty, generuj_konkretne_akordy, generuj_optymalne_akordy
 
@@ -10,23 +7,19 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         form_type = request.form.get('form_type')
-
         if form_type == 'warianty':
             tonika = request.form.get('tonika')
             typ = request.form.get('typ')
             obraz = generuj_wszystkie_warianty(tonika, typ)
             return render_template("index.html", obraz=obraz, tonika=tonika, typ=typ)
-
         elif form_type == 'konkretne':
             dane = request.form.get('akordy_konkretne')
             obraz = generuj_konkretne_akordy(dane)
             return render_template("index.html", obraz=obraz)
-
         elif form_type == 'optymalne':
             dane = request.form.get('akordy_optymalne')
             obraz = generuj_optymalne_akordy(dane)
             return render_template("index.html", obraz=obraz)
-
     return render_template("index.html")
 
 if __name__ == '__main__':
