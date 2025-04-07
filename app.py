d901abc50d89f190aca86857a713a577b7ab38f7
pawelsmykla <pawelsmykla@gmail.com>
2025-04-08 00:13:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from generator import uruchom_gui
-
 if __name__ == "__main__":
-    uruchom_gui()
+    import os
+    if os.environ.get("RENDER") == "true":
+        # Render użyje wersji webowej
+        from flask import Flask, render_template
+        app = Flask(__name__)
+
+        @app.route("/")
+        def home():
+            return render_template("index.html")
+
+        app.run(host="0.0.0.0", port=10000)
+    else:
+        uruchom_gui()
